4a213685e3f30408cd51ba2e5a5ae753f9214c50
Silentrader-cpu <abhisheksharma.work@icloud.com>
2026-01-08 13:59:36 +0000

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,1 +1,20 @@
+-- ================================
+-- PRODUCT PERFORMANCE
+-- ================================
+
+-- Revenue by product
+SELECT
+  product,
+  SUM(price * quantity) AS revenue
+FROM sales
+GROUP BY product
+ORDER BY revenue DESC;
+
+-- Revenue by category
+SELECT
+  category,
+  SUM(price * quantity) AS revenue
+FROM sales
+GROUP BY category
+ORDER BY revenue DESC;
 
